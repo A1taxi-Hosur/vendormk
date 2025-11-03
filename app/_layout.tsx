f5a4fbb173c1e330ffe8d43928d104e4d602d292
A1taxi-Hosur <a1taxihsr@gmail.com>
2025-11-03 12:18:52 +0530

    Added metro.config.js

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,24 @@
+import { useEffect } from 'react';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { useFrameworkReady } from '@/hooks/useFrameworkReady';
+import * as SplashScreen from 'expo-splash-screen';
+import 'react-native-gesture-handler';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   useFrameworkReady();
 
+  useEffect(() => {
+    SplashScreen.hideAsync();
+  }, []);
+
   return (
     <>
-      <Stack screenOptions={{ headerShown: false }}>
+      <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
+        <Stack.Screen name="+not-found" options={{ headerShown: false }} />
       </Stack>
       <StatusBar style="auto" />
     </>
