7281730bc78b864cc4d32ad3abed2537c65a6441
A1taxi-Hosur <a1taxihsr@gmail.com>
2025-11-03 12:42:18 +0530

    Added index.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,20 +1,13 @@
 import 'react-native-gesture-handler';
-import { useEffect } from 'react';
 import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
 import { useFrameworkReady } from '@/hooks/useFrameworkReady';
-
-SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   useFrameworkReady();
 
-  useEffect(() => {
-    SplashScreen.hideAsync();
-  }, []);
-
   return (
     <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="index" />
       <Stack.Screen name="(tabs)" />
       <Stack.Screen name="+not-found" />
     </Stack>
